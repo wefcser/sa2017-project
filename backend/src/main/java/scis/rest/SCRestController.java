6861f160524a1ad4d05b52546584947e7ac3052f
wefcser <2432963477@qq.com>
2017-06-15 12:02:54 +0800

    get element value

--- backend/src/main/java/scis/rest/SCRestController.java
+++ backend/src/main/java/scis/rest/SCRestController.java
@@ -37,36 +37,36 @@ public class SCRestController {
     }
 
     @RequestMapping(value = "", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<Collection<SC>> addSC(@RequestBody @Valid SC sc, BindingResult bindingResult, UriComponentsBuilder ucBuilder){
+    public ResponseEntity<SC> addSC(@RequestBody @Valid SC sc, BindingResult bindingResult, UriComponentsBuilder ucBuilder){
         System.out.println("-----------------------------post");
         BindingErrorsResponse errors = new BindingErrorsResponse();
         HttpHeaders headers = new HttpHeaders();
         if(bindingResult.hasErrors() || (sc == null)){
             errors.addAllErrors(bindingResult);
             headers.add("errors", errors.toJSON());
-            return new ResponseEntity<Collection<SC>>(headers, HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<SC>(headers, HttpStatus.BAD_REQUEST);
         }
         this.scService.saveSC(sc);
+        System.out.println("-----------------------------post"+sc.getNo().toString());
         //headers.setLocation(ucBuilder.path("/sc/{id}").buildAndExpand(sc.getId()).toUri());
-        Collection<SC> SCs = this.scService.findAllSC();
-        if(SCs.isEmpty()){
-            return new ResponseEntity<Collection<SC>>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<Collection<SC>>(SCs, HttpStatus.OK);
+        return new ResponseEntity<SC>(sc, headers, HttpStatus.CREATED);
     }
 
     @RequestMapping(value = "/{scId}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<Collection<SC>> updateVet(@PathVariable("scId") int scId, @RequestBody @Valid SC sc, BindingResult bindingResult){
+    public ResponseEntity<SC> updateSC(@PathVariable("scId") int scId, @RequestBody @Valid SC sc, BindingResult bindingResult){
+        System.out.println("-----------------------------put");
         BindingErrorsResponse errors = new BindingErrorsResponse();
         HttpHeaders headers = new HttpHeaders();
         if(bindingResult.hasErrors() || (sc == null)){
             errors.addAllErrors(bindingResult);
             headers.add("errors", errors.toJSON());
-            return new ResponseEntity<Collection<SC>>(headers, HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<SC>(headers, HttpStatus.BAD_REQUEST);
         }
+        System.out.println("-----------------------------1put"+scId);
         SC currentSC = this.scService.findById(scId);
         if(currentSC == null){
-            return new ResponseEntity<Collection<SC>>(HttpStatus.NOT_FOUND);
+            System.out.println("null");
+            return new ResponseEntity<SC>(HttpStatus.NOT_FOUND);
         }
         currentSC.setNo(sc.getNo());
         currentSC.setName(sc.getName());
@@ -74,25 +74,21 @@ public class SCRestController {
         currentSC.setCourse(sc.getCourse());
         currentSC.setGrade(sc.getGrade());
         this.scService.saveSC(currentSC);
-        Collection<SC> SCs = this.scService.findAllSC();
-        if(SCs.isEmpty()){
-            return new ResponseEntity<Collection<SC>>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<Collection<SC>>(SCs, HttpStatus.OK);
+        System.out.println("-----------------------------2put"+currentSC.getNo().toString());
+        return new ResponseEntity<SC>(currentSC, HttpStatus.NO_CONTENT);
     }
 
     @RequestMapping(value = "/{scId}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @Transactional
-    public ResponseEntity<Collection<SC>> deleteVet(@PathVariable("scId") int scId){
+    public ResponseEntity<Void> deleteSC(@PathVariable("scId") int scId){
+        System.out.println("-----------------------------delete");
         SC sc = this.scService.findById(scId);
         if(sc == null){
-            return new ResponseEntity<Collection<SC>>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
         }
+        System.out.println("-----------------------------1delete"+scId);
         this.scService.deleteSC(sc);
-        Collection<SC> SCs = this.scService.findAllSC();
-        if(SCs.isEmpty()){
-            return new ResponseEntity<Collection<SC>>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<Collection<SC>>(SCs, HttpStatus.OK);
+        System.out.println("-----------------------------2delete"+sc.getNo().toString());
+        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
     }
 }
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 import {SCService} from './sc.service';
 import {SC} from './sc';
 import 'rxjs/Rx';
@@ -13,7 +13,10 @@ import {log} from "util";
 export class AppComponent {
   errorMessage: string;
   scs: SC[];
-  sc: SC;
+  @Input() sc: SC;
+  response_status: number;
+  add_success: boolean = false;
+  delete_success: boolean = false;
 
   constructor(private router: Router, private scService: SCService) {
   }
@@ -24,17 +27,20 @@ export class AppComponent {
       error => this.errorMessage = <any> error);
   }
 
-  addOne() {
+  addOne(sc) {
     //window.open("temp.html");
-    this.sc.id = null;
-    this.sc.no = window.opener.document.getElementById("sc_no").value;
-    this.sc.name = window.opener.document.getElementById("sc_name").value;
-    this.sc.depart = window.opener.document.getElementById("sc_depart").value;
-    this.sc.course = window.opener.document.getElementById("sc_course").value;
-    this.sc.grade = null;
-    this.scService.addSC(this.sc).subscribe(
-      scs => this.scs = scs,
-      error => this.errorMessage = <any> error);
+    sc.id = null;
+    sc.no = window.opener.document.getElementById("sc_no").value;
+    sc.name = window.opener.document.getElementById("sc_name").value;
+    sc.depart = window.opener.document.getElementById("sc_depart").value;
+    sc.course = window.opener.document.getElementById("sc_course").value;
+    sc.grade = null;
+    this.scService.addSC(sc).subscribe(
+      new_sc => {
+        this.add_success = true;
+      },
+      error => this.errorMessage = <any>error);
+    this.closeDiv('AddOne');
   }
 
   importFile() {
@@ -47,16 +53,79 @@ export class AppComponent {
   }
 
   delete(sc: SC) {
-
+    this.scService.deleteSC(sc.id.toString()).subscribe(
+      response => {
+        this.response_status = response;
+        if (this.response_status === 204) {
+          this.delete_success = true;
+          this.sc = <SC>{};
+        }
+      },
+      error => this.errorMessage = <any> error);
   }
 
   edit(sc: SC) {
-
+    sc.id = parseInt(document.getElementsByTagName("input")[4].value);
+    sc.no = document.getElementsByTagName("input")[5].value;
+    sc.name = document.getElementsByTagName("input")[6].value;
+    sc.depart = document.getElementsByTagName("input")[7].value;
+    sc.course = document.getElementsByTagName("input")[8].value;
+    this.scService.updateSC(sc.id.toString(), sc).subscribe(
+      get_result,
+      error => this.errorMessage = <any> error
+    );
+    function get_result(update_status) {
+      if (update_status.status === 204) {
+        return console.log('update success');
+      } else {
+        return console.log('update failed');
+      }
+    }
+    this.closeDiv('EditOne');
   }
 
   grade(sc: SC) {
 
   }
 
+  showDiv(id,sc: SC) {
+    var Idiv = document.getElementById(id);
+    Idiv.style.display = "block";
+    //以下部分要将弹出层居中显示
+    Idiv.style.left=(document.documentElement.clientWidth-Idiv.clientWidth)/2+document.documentElement.scrollLeft+"px";
+    Idiv.style.top =(document.documentElement.clientHeight-Idiv.clientHeight)/2+document.documentElement.scrollTop-50+"px";
 
+    //以下部分使整个页面至灰不可点击
+    var procbg = document.createElement("div"); //首先创建一个div
+    procbg.setAttribute("id","mybg"); //定义该div的id
+    procbg.style.background = "#000000";
+    procbg.style.width = "100%";
+    procbg.style.height = "100%";
+    procbg.style.position = "fixed";
+    procbg.style.top = "0";
+    procbg.style.left = "0";
+    procbg.style.zIndex = "500";
+    procbg.style.opacity = "0.6";
+    procbg.style.filter = "Alpha(opacity=70)";
+    //背景层加入页面
+    document.body.appendChild(procbg);
+    document.body.style.overflow = "hidden"; //取消滚动条
+    //设置默认内容
+    if (id = "EditOne") {
+      document.getElementsByTagName("input")[4].value = sc.id.toString();
+      document.getElementsByTagName("input")[5].value = sc.no;
+      document.getElementsByTagName("input")[6].value = sc.name;
+      document.getElementsByTagName("input")[7].value = sc.depart;
+      document.getElementsByTagName("input")[8].value = sc.course;
+    }
+  }
+
+  closeDiv(id) {
+    var Idiv=document.getElementById(id);
+    Idiv.style.display="none";
+    document.body.style.overflow = "auto"; //恢复页面滚动条
+    var body = document.getElementsByTagName("body");
+    var mybg = document.getElementById("mybg");
+    body[0].removeChild(mybg);
+  }
 }
